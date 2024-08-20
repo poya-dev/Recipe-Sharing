e90cccb5bdd3ca703c6ef76f567d88e777555516
poya-dev <poya.kpu.2020@gmail.com>
2024-08-20 14:04:54 -0700

    Resolved model conflicts and added Pillow for image

--- backend/users/models.py
+++ backend/users/models.py
@@ -4,3 +4,5 @@ from django.db import models
 class User(AbstractUser):
     profile_picture = models.ImageField(upload_to='profiles', null=True, blank=True)
     bio = models.TextField(null=True, blank=True)
+    groups = models.ManyToManyField('auth.Group', related_name='custom_user_set')
+    user_permissions = models.ManyToManyField('auth.Permission', related_name='custom_user_permissions')
