cabe2618bd92e08ef36c87690ec9838579a31770
poya-dev <poya.kpu.2020@gmail.com>
2024-08-20 13:39:19 -0700

    Implemented views for RecipeViewSet, CommentViewSet and UserViewSet

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Recipe, Comment
+from .serializers import RecipeSerializer, CommentSerializer
 
-# Create your views here.
+class RecipeViewSet(viewsets.ModelViewSet):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
