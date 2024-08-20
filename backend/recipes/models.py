11bc6f5d813931ad77afddfddb176fb659234f64
poya-dev <poya.kpu.2020@gmail.com>
2024-08-20 12:55:22 -0700

    Defined Recipe and Comment models

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Recipe(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    ingredients = models.TextField()
+    instructions = models.TextField()
+    image = models.ImageField(upload_to='recipes', null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipes')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    text = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='comments')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.recipe.title}"
