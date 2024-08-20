cf09bc214e7ff9955f020db327d220d4d039eee3
poya-dev <poya.kpu.2020@gmail.com>
2024-08-20 13:15:57 -0700

    Defined model for User, also added serializer for User

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    profile_picture = models.ImageField(upload_to='profiles', null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
