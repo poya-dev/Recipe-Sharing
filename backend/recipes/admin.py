ffe28b03097a4da928132dd0e44bd392a0059202
poya-dev <poya.kpu.2020@gmail.com>
2024-08-21 13:17:00 -0700

    Registered Recipe and Comment in admin

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Recipe, Comment
 
-# Register your models here.
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'created_at', 'updated_at')
+    search_fields = ('title', 'description')
+    list_filter = ('created_at', 'user')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('text', 'user', 'recipe', 'created_at')
+    search_fields = ('text',)
+    list_filter = ('created_at', 'user', 'recipe')
