772fb426419a4690cbf8af614aa533f467841557
poya-dev <poya.kpu.2020@gmail.com>
2024-08-21 13:23:17 -0700

    Added custom permission to Recipe and Comment viewsets

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,11 +1,12 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets
 from .models import Recipe, Comment
 from .serializers import RecipeSerializer, CommentSerializer
+from .permissions import IsAuthenticatedAndCreateAllowed
 
 class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, permissions.DjangoModelPermissions]
+    permission_classes = [IsAuthenticatedAndCreateAllowed]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
@@ -13,7 +14,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, permissions.DjangoModelPermissions]
+    permission_classes = [IsAuthenticatedAndCreateAllowed]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
