de0923222b8a126103747a824efe7be178531730
poya-dev <poya.kpu.2020@gmail.com>
2024-08-22 12:00:23 -0700

    Configured routing using react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,24 @@
-function App() {
-  return <div className="App"></div>;
-}
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { CssBaseline, Container } from '@mui/material';
+
+import Navbar from './components/Navbar';
+
+const App = () => {
+  return (
+    <Router>
+      <CssBaseline />
+      <Navbar />
+      <Container maxWidth="lg" sx={{ mt: 4 }}>
+        <Routes>
+          <Route path="/" element={<p>Home Page</p>} />
+          <Route path="/recipes" element={<p>Recipes</p>} />
+          <Route path="/login" element={<p>Login</p>} />
+          <Route path="/sign-up" element={<p>Sign-Up</p>} />
+        </Routes>
+      </Container>
+    </Router>
+  );
+};
 
 export default App;
