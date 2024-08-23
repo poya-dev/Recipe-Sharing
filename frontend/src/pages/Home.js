21f23794dbe65a433094980f01a4355b75aa7099
poya-dev <poya.kpu.2020@gmail.com>
2024-08-23 10:50:49 -0700

    Used recipeStore data instead of dummy data

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,17 +1,25 @@
-// src/pages/Home.js
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Typography } from '@mui/material';
-import RecipeList from '../components/RecipeList';
 
-import { RECIPE_LIST } from '../dummy';
+import RecipeList from '../components/RecipeList';
+import useRecipeStore from '../store/recipeStore';
 
 const Home = () => {
+  const { recipes, loading, error, fetchAllRecipes } = useRecipeStore();
+
+  useEffect(() => {
+    fetchAllRecipes();
+  }, [fetchAllRecipes]);
+
+  if (loading) return <Typography>Loading recipes...</Typography>;
+  if (error) return <Typography color="error">{error}</Typography>;
+
   return (
     <>
       <Typography variant="h4" component="h1" gutterBottom>
         All Recipes
       </Typography>
-      <RecipeList recipes={RECIPE_LIST} />
+      <RecipeList recipes={recipes} />
     </>
   );
 };
